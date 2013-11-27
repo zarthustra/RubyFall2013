35b286f8341705d782edae0904da5118c50ffc4c
Jordan <jordan@jordan-SN12E200.(none)>
2013-11-26 17:55:26 -0800

    week 7 hw

---
+++ week7/homework/pirate_translator.rb
@@ -0,0 +1,12 @@
+class PirateTranslator
+
+	def say (pirate_talk)
+		#def initialize?
+		puts pirate_talk
+	end
+
+	def translate(pirate_talk = '')
+		#@pirate_talk= 
+		"Ahoy Matey\n Shiber Me Timbers You Scurvey Dogs!!"
+	end
+end
